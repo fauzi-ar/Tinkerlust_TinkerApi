3a9fc70b2e9b7789da1b8d6b29af2d4e9fcab2ab
uziau <fuzzy.ar@gmail.com>
2016-11-22 10:06:39 +0700

    fix the upgrade script (it created a wrong table

--- app/code/local/Tinkerlust/TinkerApi/sql/oauth2_setup/mysql4-upgrade-0.1.0-0.2.0.php
+++ app/code/local/Tinkerlust/TinkerApi/sql/oauth2_setup/mysql4-upgrade-0.1.0-0.2.0.php
@@ -8,11 +8,5 @@ $installer->run("
 	CREATE TABLE tinkerlust_oauth2_accesstokens (access_token VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT access_token_pk PRIMARY KEY (access_token));
 ");
 
-//install oauth2 refersh token
-$installer->run("
-	DROP TABLE IF EXISTS tinkerlust_oauth2_refreshtokens;
-	CREATE TABLE oauth_refresh_tokens (refresh_token VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT refresh_token_pk PRIMARY KEY (refresh_token));
-");
-
 $installer->endSetup();
 ?>
